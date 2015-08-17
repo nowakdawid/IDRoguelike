fcaca39868a6f3f2a161e86b62e0e773f2f5554f
izasemczuk <it.semczuk@gmail.com>
2015-08-17 15:32:47 +0200

    Classes Map and Player

---
+++ src/test/java/Player.java
@@ -0,0 +1,24 @@
+/**
+ * Created by izasemczuk on 2015-08-17.
+ */
+public class Player {
+    private int currentPositionX = 17;
+    private int currentPositionY = 17;
+
+
+    public int getCurrentPositionX() {
+        return currentPositionX;
+    }
+
+    public void setCurrentPositionX(int currentPositionX) {
+        this.currentPositionX = currentPositionX;
+    }
+
+    public int getCurrentPositionY() {
+        return currentPositionY;
+    }
+
+    public void setCurrentPositionY(int currentPositionY) {
+        this.currentPositionY = currentPositionY;
+    }
+}
